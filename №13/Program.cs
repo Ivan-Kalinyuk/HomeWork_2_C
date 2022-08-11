6e978e2509d7bdbd5eadacce22d9daba96a684d2
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-12 00:13:21 +0300

    Написали программу, показывающую третью цифру натурального числа, которое ранее ввел пользователь или сообщающую, что третьей цифры нет (число меньше 100)

---
+++ №13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите любое натуральное число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int res1 = a;
+if (a<100) 
+{
+    Console.Write("Третьей цифры нет ");
+}
+else
+{
+    while (res1 > 999 )
+    {
+        res1 = res1/10;
+    }
+    int rem1 = res1 % 10;
+    Console.Write("Третья цифра Вашего числа = ");
+    Console.WriteLine(rem1);
+}
+
+ 
