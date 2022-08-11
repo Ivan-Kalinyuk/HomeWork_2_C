27110694b043879c74c23fc7b11a174baab9d8c1
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-11 22:34:55 +0300

    Написали программу, показывающую вторую цифру трехзначного числа, которое ранее ввел пользователь

---
+++ №10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int res1 = a/10;
+int rem2 = res1 % 10;
+Console.Write("Вторая цифра Вашего числа = ");
+Console.WriteLine(rem2);
