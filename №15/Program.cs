c4d434f952c7f2abce52eecd7b4f5ed6e81a94da
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-08-12 17:56:00 +0300

    Создали программу, проверяющую день недели на будний/выходной

---
+++ №15/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число от 1 до 7 (1 - пн, 2 - вт, 3 - ср, 4 - чт, 5 - пт, 6 - сб, 7 - вс) = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 6) Console.WriteLine("Выбранный Вами день - будний");
+if (a > 5) Console.WriteLine("Выбранный Вами день - выходной");
